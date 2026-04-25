eb3822a82ebc4b23c55bd464eb41a6b1e8aa2a78
Vikash <vk6484412@gmail.com>
2026-04-25 05:54:45 +0530

    minor changes

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 
 const invoiceRoutes = require('./routes/invoiceRoutes');
 const itemRoutes = require('./routes/itemRoutes');
-dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -13,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 // Database connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/healthychef-invoices')
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 

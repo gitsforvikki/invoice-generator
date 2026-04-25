6de5e10c859989aa025187a94d5999c7d74a4960
Vikash <vk6484412@gmail.com>
2026-04-25 06:45:44 +0530

    cors error fixed

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 // Database connection
 mongoose.connect(process.env.MONGODB_URI)
